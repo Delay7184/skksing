e9c69f77db045f6aa075d0fe0341c0b9ef598b6c
Delay7184 <155452307+Delay7184@users.noreply.github.com>
2025-08-06 18:08:10 +0800

    Update split_process_name_ruleset.py

--- split_process_name_ruleset.py
+++ split_process_name_ruleset.py
@@ -1,19 +1,25 @@
 import json
 import sys
 
-def split_process_name_ruleset(input_file, output_file):
+def split_process_rules(input_file, output_file):
     with open(input_file, 'r', encoding='utf-8') as f:
         data = json.load(f)
 
     new_rules = []
     for rule in data.get("rules", []):
-        if "process_name" in rule:
-            process_rule = {"process_name": rule["process_name"]}
-            rule_without_process = {k: v for k, v in rule.items() if k != "process_name"}
-            if rule_without_process:
-                new_rules.append(rule_without_process)
-            new_rules.append(process_rule)
-        else:
+        if "process_name" in rule or "process_path" in rule:
+            # 分离 process_name
+            if "process_name" in rule:
+                process_name_rule = {"process_name": rule["process_name"]}
+                new_rules.append(process_name_rule)
+                del rule["process_name"]
+
+            if "process_path" in rule:
+                process_path_rule = {"process_path": rule["process_path"]}
+                new_rules.append(process_path_rule)
+                del rule["process_path"]
+
+        if rule:
             new_rules.append(rule)
 
     data["rules"] = new_rules
@@ -23,6 +29,6 @@ def split_process_name_ruleset(input_file, output_file):
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("python split_process_name_ruleset.py input.json output.json")
+        print("python split_process_rules.py input.json output.json")
     else:
-        split_process_name_ruleset(sys.argv[1], sys.argv[2])
+        split_process_rules(sys.argv[1], sys.argv[2])
